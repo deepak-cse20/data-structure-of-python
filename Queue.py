2c2fa9bcfdca4c543c26dc82201d1abf2ffc9892
deepak-cse20 <deepxzz20@gmail.com>
2026-07-30 12:51:25 +0530

    Add files via upload

---
+++ Queue.py
@@ -0,0 +1,28 @@
+
+operator=set(['+','-','(',')','*','/','^'])
+priority={'+':1,'-':1,'*':2,'/':2,'^':3}
+
+def infix_To_Postfix(expression):
+    stack=[]
+    output=""
+    for ch in expression:
+        if ch not in operator:
+            output += ch
+        elif ch=='(':
+            stack.append('(')
+        elif ch==')':
+            while stack and  stack[-1]!='(':
+                output +=stack.pop()
+            stack.pop()
+        else:
+            while stack and stack[-1]!='(' and priority[ch]<=priority[stack[-1]]:
+                output +=stack.pop()
+            stack.append(ch)
+    while stack:
+        output +=stack.pop()
+    return output
+expression=input("enter infix expression: ")
+result=infix_To_Postfix(expression)
+print(f"Result :{result}")
+
+   
